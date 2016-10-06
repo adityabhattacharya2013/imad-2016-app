6236d285b6149852041dbb3388867c6f86bad7f9
adityabhattacharya2013 <aditya.bhattacharya2013@vit.ac.in>
2016-10-06 10:29:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,13 @@ function moveRight()
 {
     marginLeft=marginLeft+1;
     img.style.marginLeft=marginLeft+'px';
+    if(marginLeft>=100)
+    {
+        for(marginLeft=100;marginLeft>0;marginLeft--)
+        {
+            img.style.marginLeft=marginLeft+'px';
+        }
+    }
 }
 img.onclick=function()
 {
